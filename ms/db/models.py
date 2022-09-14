841962b07d16ee8eff824cc7d8cc8396a4eccf40
v <post@i3o.eu>
2022-09-14 10:26:59 +0200

    added unit model and rel to product

--- ms/db/models.py
+++ ms/db/models.py
@@ -21,19 +21,34 @@ class BaseForm(ModelForm):
             return session
 
 
+class Unit(db.Model):
+
+    __tablename__ = "units"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    by_piece = db.Column(db.Boolean, nullable=False)
+    shortname = db.Column(db.String(128), nullable=False)
+    longname = db.Column(db.String(128), nullable=False)
+    products = db.relationship("Product", backref="unit", lazy=True)
+
+
 class Product(db.Model):
 
     __tablename__ = "products"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(128), nullable=False)
-    by_piece = db.Column(db.Boolean, nullable=False)
+    last_distribution = db.Column(db.DateTime, nullable=True)
     last_update = db.Column(db.DateTime, nullable=False)
+    info = db.Column(db.String(128), nullable=True)
 
-    def __init__(self, name, by_piece):
+    unit_id = db.Column(db.Integer, db.ForeignKey("units.id"), nullable=False)
+
+    def __init__(self, name, unit_id, info=None):
 
         self.name = name
-        self.by_piece = by_piece
+        self.unit_id = unit_id
+        self.info = info
         self.last_update = datetime.datetime.now()
 
 
--- tests/db/test_models.py
+++ tests/db/test_models.py
@@ -1,29 +1,68 @@
 import json
 from random import choice
 
-from ms.db.models import Product
+from pytest import mark
+
+from ms.db.models import Product, Unit, db
 
 
-def test_add_product(test_client, product):
+def test_add_unit(test_app):
 
-    item = {"name": product["name"], "by_piece": product["by_piece"]}
+    assert True
+
+
+def test_unit_product(test_app):
+
+    kg = Unit(shortname="kg", by_piece=False, longname="Kilogramm")
+    st = Unit(shortname="st", by_piece=True, longname="Stück")
+    db.session.add(kg)
+    db.session.add(st)
+    product = Product(name="Kartoffel", unit_id=1)
+    db.session.add(product)
+    product = Product(name="Kohlrabi", unit_id=2)
+    db.session.add(product)
+    product = Product(name="Mangold", unit_id=1)
+    db.session.add(product)
+
+    db.session.commit()
+
+    products = Product.query.all()
+    units = Unit.query.all()
+
+    for product in products:
+        assert product.unit.longname
+
+
+def _add_product_post(
+    name: str = "Kohlrabi", unit_id: int = 2, info: str = None, test_client=None
+):
+
+    item = {"name": name, "info": info, "unit_id": unit_id}
     data = json.dumps(item)
     response = test_client.post(
         "/products/new", data=data, content_type="application/json"
     )
+    return response
 
-    assert item["name"] in response.text
+
+def test_add_product(test_client, product):
+
+    response = _add_product_post(
+        name=product["name"], unit_id=2, info=None, test_client=test_client
+    )
+
+    assert product["name"] in response.text
     assert response.status_code == 201
 
 
 def test_added_products_in_database(test_client, products):
 
-    item = {"name": "chickpeas", "by_piece": True}
-    data = json.dumps(item)
-    test_client.post("/products/new", data=data, content_type="application/json")
-    item = {"name": "oranges", "by_piece": False}
-    data = json.dumps(item)
-    test_client.post("/products/new", data=data, content_type="application/json")
+    response = _add_product_post(
+        name="chickpeas", unit_id=1, info=None, test_client=test_client
+    )
+    response = _add_product_post(
+        name="oranges", unit_id=2, info=None, test_client=test_client
+    )
 
     chickpeas = Product.query.filter_by(name="chickpeas").first()
     oranges = Product.query.filter_by(name="oranges").first()
