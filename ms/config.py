2863bc707ab12c06793e47f98b6d1289883eb680
v <post@i3o.eu>
2022-09-14 18:41:10 +0200

    fixed missing CSRF Key for testing

--- ms/config.py
+++ ms/config.py
@@ -5,4 +5,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/database.sqlite"
-    CSRF_SECRET_KEY = os.environ.get("CSRF_SECRET_KEY").encode()
+    CSRF_SECRET_KEY = os.environ.get("CSRF_SECRET_KEY")
+    if CSRF_SECRET_KEY:
+        CSRF_SECRET_KEY = CSRF_SECRET_KEY.encode()
