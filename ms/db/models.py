64d7a35740c5837651d8616dcbeee1d1d850598b
v <post@i3o.eu>
2022-09-15 16:33:05 +0200

    adding station and TimestampMixin

--- ms/db/models.py
+++ ms/db/models.py
@@ -5,6 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class TimestampMixin(object):
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
+
+
 class Unit(db.Model):
 
     __tablename__ = "units"
@@ -16,15 +21,15 @@ class Unit(db.Model):
     products = db.relationship("Product", backref="unit", lazy=True)
 
 
-class Product(db.Model):
+class Product(TimestampMixin, db.Model):
 
     __tablename__ = "products"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(128), nullable=False)
-    last_distribution = db.Column(db.DateTime, nullable=True)
+    info = db.Column(db.String(128))
+    last_distribution = db.Column(db.DateTime)
     last_update = db.Column(db.DateTime, nullable=False)
-    info = db.Column(db.String(128), nullable=True)
 
     unit_id = db.Column(db.Integer, db.ForeignKey("units.id"), nullable=False)
 
@@ -34,3 +39,21 @@ class Product(db.Model):
         self.unit_id = unit_id
         self.info = info
         self.last_update = datetime.datetime.now()
+
+
+class Station(TimestampMixin, db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
+    info = db.Column(db.String(128), nullable=False)
+    delivery_order = db.Column(db.Integer)
+    members_full = db.Column(db.Integer, nullable=False)
+    members_half = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, **k):
+
+        self.name = k["name"]
+        self.info = k["info"]
+        self.delivery_order = k["delivery_order"]
+        self.members_full = k["members_full"]
+        self.members_half = k["members_half"]
--- tests/db/test_models.py
+++ tests/db/test_models.py
@@ -1,4 +1,6 @@
-from ms.db.models import Product, Unit, db
+import datetime
+
+from ms.db.models import Product, Station, Unit, db
 
 
 def test_add_unit(test_app):
@@ -6,7 +8,7 @@ def test_add_unit(test_app):
     assert True
 
 
-def test_unit_product(test_app):
+def test_adding_unit_product(test_app):
 
     kg = Unit(shortname="kg", by_piece=False, longname="Kilogramm")
     st = Unit(shortname="st", by_piece=True, longname="Stück")
@@ -40,3 +42,59 @@ def test_consistency_of_db_model():
 
     assert "Kartoffel" == potatoe.name
     assert "Mangold" == mangold.name
+
+
+def test_adding_station(test_app):
+
+    station_1 = Station(
+        name="Station Superstar",
+        delivery_order=2,
+        info="Beste Station der Welt!",
+        members_full=32,
+        members_half=12,
+    )
+    station_2 = Station(
+        name="Station3000",
+        delivery_order=1,
+        info="Megabeste Station der Superwelt!",
+        members_full=12,
+        members_half=13,
+    )
+
+    db.session.add(station_1)
+    db.session.add(station_2)
+
+    db.session.commit()
+
+    station = Station.query.get(1)
+    assert station == station_1
+    station = Station.query.get(2)
+    assert station == station_2
+
+
+def test_edit_station_save_to_hist(test_app):
+
+    # get station
+    station = Station.query.get(1)
+    assert station.name == "Station Superstar"
+    first_update_time = station.updated
+    members_full = station.members_full
+
+    # make changes
+    station.name = "Superstation Megagood NEW"
+    station.members_full = 35
+    db.session.commit()
+
+    # expect changes
+    station_new = Station.query.get(1)
+    assert station_new.name != "Station Superstar"
+    assert station_new.members_full != members_full
+
+    # expect updated time to be changed
+    assert station_new.updated != first_update_time
+
+    # expect previous station was saved to history
+    entry = StationHistory.query.filter_by(
+        name="Station Superstar", date=first_update_time, members_full=members_full
+    ).first()
+    assert entry
