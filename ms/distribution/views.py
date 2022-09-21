3766f10a4de02ee4b4c40a4f0af7424bebb50088
v <post@i3o.eu>
2022-09-21 12:14:49 +0200

    distribution now with multiple units

--- ms/distribution/views.py
+++ ms/distribution/views.py
@@ -8,7 +8,7 @@ from flask import (
     url_for,
 )
 
-from ms.db.models import Product
+from ms.db.models import Product, Station, Unit
 
 distribution = Blueprint("distribution", __name__)
 
@@ -25,11 +25,9 @@ def product(p_id):
     product = Product.query.get_or_404(p_id)
 
     if not product.units:
-
         abort(404)
 
     if len(product.units) == 1:
-
         return redirect(
             url_for(
                 "distribution.distribute",
@@ -45,7 +43,26 @@ def product(p_id):
 @distribution.route("/<int:p_id>/<p_unit_shortname>")
 def distribute(p_id: int, p_unit_shortname: str):
 
-    return ""
+    product = Product.query.get_or_404(p_id)
+    unit = Unit.query.filter_by(shortname=p_unit_shortname).first()
+
+    if not product or not unit:
+        abort(404)
+
+    stations = Station.query.all()
+    station_sums = {
+        "full": sum(station.members_full for station in stations),
+        "half": sum(station.members_half for station in stations),
+    }
+    station_sums["total"] = station_sums["full"] + station_sums["half"]
+
+    return render_template(
+        "distribution/distribute.html",
+        product=product,
+        unit=unit,
+        stations=stations,
+        station_sums=station_sums,
+    )
 
 
 @distribution.route("/<int:productid>")
